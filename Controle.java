cd37f096c48c912185a331c5198552fc5f64d679
GH120 <felipevieiraduarte195@gmail.com>
2024-09-21 19:45:32 -0300

    Especificações do controle

--- Controle.java
+++ Controle.java
@@ -1,5 +1,16 @@
 public class Controle {
 
+    //create database {database: {pagina1:{...}, pagina2{...}}}
+
+
+
+    //receber escalonamento r1(x with pagelock)w1(y with tablelock)w2(z)c2w3(y)r3(z)c1c3 -> retorna escalonamento no output
+        //recebe string
+        //Transforma em linkedList de operações 
+        //roda o Protocolo.rodar(linkedList de operações)
+        //Usa o escalonamento desse protocolo para criar uma string
+        //Printa no output
+
     void selecionarTransacao(){
         
     }
